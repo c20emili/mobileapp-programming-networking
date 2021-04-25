61e36dd6a202b4e5addd3550af318a5baae12e12
c20emili <c20emili@student.his.se>
2021-04-25 16:33:57 +0200

    created an ArrayAdapter for my ListView

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    private String mountName;
+    private String[] mountName;
     private ArrayList<String> mountList = new ArrayList<>(Arrays.asList(mountName));
+    private ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.list_view);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

0124c8d493aa493403e834a84b175ae8796eab00
c20emili <c20emili@student.his.se>
2021-04-25 17:40:08 +0200

    Created the Mountain class and remade my adapter in the correct place

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] mountName;
-    private ArrayList<String> mountList = new ArrayList<>(Arrays.asList(mountName));
-    private ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.list_view);
+    private ArrayList<Mountain> mountainArrayList = new ArrayList<>();
 
+
+    ListView myListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mountainArrayList.add(new Mountain("Matterhorn","Alps",4478));
+        mountainArrayList.add(new Mountain("Mont Blanc","Alps",4808));
+        mountainArrayList.add(new Mountain("Denali","Alaska",6190));
+        ArrayAdapter<Mountain> mountainArrayAdapter = new ArrayAdapter<Mountain>(this, R.layout.list_item, R.id.list_item, mountainArrayList);
+
+        myListView = findViewById(R.id.my_listview);
+        myListView.setAdapter(mountainArrayAdapter);
     }
 }
---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,49 @@
+package com.example.networking;
+
+public class Mountain {
+    private String name;
+    private String location;
+    private int height;
+    //default
+    public Mountain()
+    {
+        name = "Missing name";
+        location = "Missing Location";
+        height = -1;
+    }
+    //new mountains
+    public Mountain(String n, String l, int h) {
+        name = n;
+        location = l;
+        height = h;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setLocation(String l) {
+        location = l;
+    }
+
+    public void setHeight(int h) {
+        height = h;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}
